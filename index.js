e3b488e2c62c0855d5e0cd339dd208c2c598cb7d
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-14 15:35:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Write your code in this file!
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideHeight){
+  if(rideHeight<=400){
 
+  }else if (rideHeight > 2500) {
+
+  }else if (rideHeight >2000) {
+
+  }
 }
